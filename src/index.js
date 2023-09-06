8cfff3ab6b001bcb4c2a9cc39fad40cc9c3e1b1a
Evgeniy Kvasov <E_kvasov@inbox.ru>
2023-09-06 08:56:45 +0500

    finish

--- src/index.js
+++ src/index.js
@@ -39,6 +39,25 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let result = '';
+    let arr = [];
+    for (i = 0; i < expr.length; i = i + 2) {
+      expr[i] === '0' ? result += '' :
+      expr[i] === '*' ? result = ' ' :
+      expr[i + 1] === '0' ? result += '.' : result += '-';
+      if (String(i).endsWith('8')) {
+        arr[Math.floor(i / 10)] = result;
+        result = '';
+      }
+    }
+
+    const str = arr.map(toDoText).join().replace(/,/g, '');
+    return str;
+}
+
+const toDoText = (item) => {
+  if (item === ' ') return ' ';
+  else return MORSE_TABLE[item];
 }
 
 module.exports = {
